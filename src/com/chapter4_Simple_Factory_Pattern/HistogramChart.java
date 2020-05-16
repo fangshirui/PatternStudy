acd0db099fe5ffcffe1fa9786c9d6a0789ea06a9
kk <fsrgyx@gmail.com>
2020-05-16 21:44:09 +0800

    小更正

--- src/com/chapter4_Simple_Factory_Pattern/HistogramChart.java
+++ src/com/chapter4_Simple_Factory_Pattern/HistogramChart.java
@@ -1,9 +1,5 @@
 package com.chapter4_Simple_Factory_Pattern;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-import jdk.nashorn.internal.parser.JSONParser;
-
-import java.sql.SQLOutput;
 
 //具体产品类
 public class HistogramChart implements Chart {
@@ -15,10 +11,5 @@ public class HistogramChart implements Chart {
     public void display() {
         System.out.println("显示柱状图!");
     }
-
-    public static void main(String[] args) {
-        System.out.println("Hi");
-    }
-
 
 }
